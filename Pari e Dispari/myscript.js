64d4826245fbfd18886c392c782d0182b99e3c40
Lex95 <andrea.lega1995@gmail.com>
2021-04-14 16:15:40 +0200

    risoluzione secondo esercizio

--- Pari e Dispari/myscript.js
+++ Pari e Dispari/myscript.js
@@ -2,13 +2,22 @@ var scelta = prompt('Scegli "pari" o "dispari":');
 var pari;
 
 if (scelta == "pari") {
-    pari = true;
+    pari = 0;
 } else if (scelta == "dispari") {
-    pari = false;
+    pari = 1;
 } else {
     alert("Non hai inserito una parola accettata!");
 }
 
 if ((pari == true) || (pari == false)) {
-    alert("bella fra");
+    var numeroUtente = parseInt(prompt("Inserisci un numero (intero) da 1 a 5:"));
+    var numeroAi = Math.ceil(Math.random() * 5);
+    if (((numeroUtente + numeroAi) % 2) == pari) {
+        document.getElementById("myId").innerHTML = "Hai vinto!";
+    } else {
+        document.getElementById("myId").innerHTML = "Hai perso!";
+    }
+    console.log(numeroUtente);
+    console.log(numeroAi);
+    console.log(numeroUtente + numeroAi);
 }
