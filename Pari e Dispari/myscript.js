b83fdac26855c0a70081e9f8953c4feb71053bd7
Lex95 <andrea.lega1995@gmail.com>
2021-04-14 16:29:12 +0200

    risoluzione secondo esercizio mediante funzioni

--- Pari e Dispari/myscript.js
+++ Pari e Dispari/myscript.js
@@ -9,15 +9,32 @@ if (scelta == "pari") {
     alert("Non hai inserito una parola accettata!");
 }
 
+function randomGenerator(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+function pariDispari(num) {
+    if ((num % 2) == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 if ((pari == true) || (pari == false)) {
     var numeroUtente = parseInt(prompt("Inserisci un numero (intero) da 1 a 5:"));
-    var numeroAi = Math.ceil(Math.random() * 5);
-    if (((numeroUtente + numeroAi) % 2) == pari) {
-        document.getElementById("myId").innerHTML = "Hai vinto!";
+    if (numeroUtente > 5 || numeroUtente < 1 || Number.isNaN(numeroUtente)){
+        alert("Numero inserito non valido!")
     } else {
-        document.getElementById("myId").innerHTML = "Hai perso!";
+        var numeroAi = randomGenerator(5);
+        var parDis = pariDispari(numeroUtente + numeroAi);
+        if (parDis == pari) {
+            document.getElementById("myId").innerHTML = "Hai vinto!";
+        } else {
+            document.getElementById("myId").innerHTML = "Hai perso!";
+        }
+        console.log(numeroUtente);
+        console.log(numeroAi);
+        console.log(numeroUtente + numeroAi);
     }
-    console.log(numeroUtente);
-    console.log(numeroAi);
-    console.log(numeroUtente + numeroAi);
 }
