12a69dd37c235f226d5980171a20c37ceb9d739c
Lex95 <andrea.lega1995@gmail.com>
2021-04-15 10:12:15 +0200

    correzione risoluzione (non avevo usato una function per il controllo)

--- Palindroma/myscript.js
+++ Palindroma/myscript.js
@@ -1,14 +1,23 @@
 var parola = prompt("Inserisci una parola:");
-var parolaContr = "";
+
+function isPalindroma(word) {
+    var wordReverse = "";
+    for (var i = (word.length -1); i >= 0; i--) {
+        wordReverse += word[i];
+    }
+    if (word === wordReverse) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 if (parola == "") {
     alert("Non hai inserito nulla!");
 }
 else {
-    for (var i = (parola.length -1); i >= 0; i--) {
-        parolaContr += parola[i];
-    }
-    if (parola === parolaContr) {
+    var palindroma = isPalindroma(parola);
+    if (palindroma) {
         document.getElementById("myId").innerHTML = "La parola " + parola + " è palindroma!";
     } else {
         document.getElementById("myId").innerHTML = "La parola " + parola + " non è palindroma!";
